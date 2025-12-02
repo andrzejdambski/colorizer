a28ee5859f814570fe116f864563fc7db805b117
yani chelah <chelahy@yahoo.com>
2025-12-02 16:06:38 +0100

    fonction finie

--- Fonctions_LAB_RGB.py
+++ Fonctions_LAB_RGB.py
@@ -1,19 +1,20 @@
 
 # RGB batch -> Lab batch
+import numpy as np
 from skimage import color
 
 def rgb_to_lab_batch(X):
     """
-    Convertit un batch RGB (N, H, W, 3) en Lab.
+    prends un array rgb; ressort un array lab en float 32
     """
     X_lab = color.rgb2lab(X)
     return X_lab
 
-# Si on veut le contraire
 # Lab batch -> RGB batch
 def lab_to_rgb_batch(X_lab):
     """
-    Convertit un batch Lab (N, H, W, 3) en RGB normalisé [0,1].
+    Convertit un batch Lab (N, H, W, 3) en RGB
+    Retourne un array (N, H, W, 3) en float32.
     """
     X_rgb = color.lab2rgb(X_lab)
     return X_rgb
