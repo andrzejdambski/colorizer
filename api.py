1a5757ce2dd0eb6fcb6aede345a9e9267079c93a
Constantin d Orleans <constantin.orleans@gmail.com>
2025-12-02 15:55:04 +0100

    jai termine une derniere fonction

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 from google.cloud import storage
 import os
+from PIL import Image
+import io
+import numpy as np
 
 def getting_file_names():
     '''
@@ -58,7 +61,31 @@ def vectorise_jpg():
     return img
 
 
+def vectorise_file_from_bucket():
+    '''
+    Pioche dans le bucket le nom des fichiers, et les vectorise un par un
+    appendant a une liste.
+    '''
+    client = storage.Client()
+    bucket = client.bucket("colorizer")
 
+    file_names = []
+    vectorised_images = []
+
+    for blob in bucket.list_blobs():
+        if blob.name.endswith('.jpg'):
+            file_names.append(blob.name)
+
+
+    for i in range(len(file_names)-9961):
+        bloby = bucket.blob(file_names[i])
+
+        img_bytes = bloby.download_as_bytes()
+        image = Image.open(io.BytesIO(img_bytes))
+        arr = np.array(image)
+        vectorised_images.append(arr)
+
+    return vectorised_images
 
     # img_bytes = bloby.download_as_bytes()
     # image = Image.open(io.BytesIO(img_bytes))
