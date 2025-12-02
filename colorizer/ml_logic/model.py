d2070efca968363cefa16397fa6b5b70c25f76cf
Nino Doinjashvili <ninodoinjashvili@gmail.com>
2025-12-02 14:36:59 +0100

    molel.py modified code de Timothe

---
+++ colorizer/ml_logic/model.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+"""code de Timothe"""
+
+# import keras
+
+# train_ds = keras.utils.image_dataset_from_directory(
+#     directory='training_data/',
+#     labels='inferred',
+#     label_mode='categorical',
+#     batch_size=32,
+#     image_size=(256, 256))
+# validation_ds = keras.utils.image_dataset_from_directory(
+#     directory='validation_data/',
+#     labels='inferred',
+#     label_mode='categorical',
+#     batch_size=32,
+#     image_size=(256, 256))
+
+# model = keras.applications.Xception(
+#     weights=None, input_shape=(256, 256, 3), classes=10)
+# model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+# model.fit(train_ds, epochs=10, validation_data=validation_ds)
