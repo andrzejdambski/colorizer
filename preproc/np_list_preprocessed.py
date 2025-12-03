c6435baaa28cf4189187a16e99c4005de1bd2c84
Nino Doinjashvili <ninodoinjashvili@gmail.com>
2025-12-03 15:35:11 +0100

    generetor

--- preproc/np_list_preprocessed.py
+++ preproc/np_list_preprocessed.py
@@ -1,6 +1,6 @@
 import numpy as np
 import glob
-from colorizer.preproc.zoom import zoom_on_cat_face
+from colorize.preproc.zoom import zoom_on_cat_face
 
 def pil_to_np(pil_image):
 
@@ -13,24 +13,24 @@ def create_local_file_paths_lists():
         for file in glob.glob(f'/home/andrzej/code/andrzejdambski/Projet_colorisation/2/CAT_0{n}/*.jpg'):
             l_jpg.append(file)
             l_jpg_name.append(file.replace(f'/home/andrzej/code/andrzejdambski/Projet_colorisation/2/CAT_0{n}/','').replace('.jpg',''))
-            
+
     l_jpg_cat = []
     l_jpg_name_cat = []
     for n in range(7):
         for file in glob.glob(f'/home/andrzej/code/andrzejdambski/Projet_colorisation/2/CAT_0{n}/*.jpg.cat'):
             l_jpg_cat.append(file)
             l_jpg_name_cat.append(file.replace(f'/home/andrzej/code/andrzejdambski/Projet_colorisation/2/CAT_0{n}/','').replace('.jpg.cat',''))
-            
+
     return l_jpg,l_jpg_name,l_jpg_cat,l_jpg_name_cat
 
 def create_list_np():
-    
+
     l_jpg,l_jpg_name,l_jpg_cat,l_jpg_name_cat = create_local_file_paths_lists()
-    
+
     l_jpg_cat = [file+'.cat' for file in l_jpg]
 
     list_np = []
-    
+
     for cat_image,file_cat in list(zip(l_jpg,l_jpg_cat)):
         pil_image = zoom_on_cat_face(cat_image,file_cat)
         if pil_image != None:
--- preproc/pipe_preproc.py
+++ preproc/pipe_preproc.py
@@ -1,10 +1,12 @@
-#importation des librairies
+# importation des librairies
 import numpy as np
 import tensorflow as tf
 import LAB as lab
 
 # Import file
-data = np.load('../../raw_data/cats_64x64_full.npy')
+data = np.load(
+    "/Users/ninadoinjashvili/code/andrzejdambski/colorizer/raw_data/cat_full_tf.npy"
+)
 
 # RGB to LAB
 lab_data = lab.rgb_to_lab(data)
@@ -18,8 +20,8 @@ L_tf = tf.data.Dataset.from_tensor_slices(L_dataset)
 AB_tf = tf.data.Dataset.from_tensor_slices(AB_dataset)
 
 # Normalisation des L et A_B dans deux datasets normalisés
-L_normalized = L_tf.map(lambda x: x / 50.0-1)
-AB_normalized = AB_tf.map(lambda x: (x / 128.0) )
+L_normalized = L_tf.map(lambda x: x / 50.0 - 1)
+AB_normalized = AB_tf.map(lambda x: (x / 128.0))
 
 # Préparation à l'entrainement en 32 tenseurs
 batch_size = 32
@@ -31,8 +33,8 @@ L_array = np.concatenate([batch.numpy() for batch in L_normalized], axis=0)
 AB_array = np.concatenate([batch.numpy() for batch in AB_normalized], axis=0)
 
 # Sauvegarder les fichiers dans le dossier raw_data
-np.save('../../raw_data/L_normalized.npy', L_array)
-np.save('../../raw_data/AB_normalized.npy', AB_array)
+np.save("../../raw_data/L_normalized.npy", L_array)
+np.save("../../raw_data/AB_normalized.npy", AB_array)
 
-#Affichage des shapes des deux datasets npy enregistrés
-print(L_array.shape , AB_array.shape)
+# Affichage des shapes des deux datasets npy enregistrés
+print(L_array.shape, AB_array.shape)
