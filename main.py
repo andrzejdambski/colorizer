02844bef9885061d0659a65d3c75979d92ce4ebe
andrzejdambski <@users.noreply.github.com>
2025-12-08 16:59:48 +0100

    ajout de la sauvegarde du model .keras en local et sur le bucket

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ from preproc.preproc import get_dataset, get_list_of_paths, preprocess
 import time
 from keras import Sequential,Input,layers
 from keras.callbacks import EarlyStopping
-from model.model import Generator, Discriminator, mae, generator_loss, train, train_step, fit
+from model.model import Generator, Discriminator, mae, generator_loss, train, train_step, fit, save_model
 import math
+from keras.models import save_model, load_model
+from google.cloud import storage
 
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 
 # "/Users/andrzej/.config/gcloud/application_default_credentials.json"
 
 # url = 'gs://colorizer/'
 # url = 'gs://catsdata/'
+BUCKET_NAME = 'catsdata'
 url = './raw_data/catsdata/'
 
 l_jpg = getting_file_names()
@@ -51,17 +54,18 @@ discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator = Discriminator(image_size=image_size)
 print(f'\n ✅ discriminator done')
 
-# checkpoint_dir = './training_checkpoints'
-# checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-# checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-#                                  discriminator_optimizer=discriminator_optimizer,
-#                                  generator=generator,
-#                                  discriminator=discriminator)
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
 
-epochs = 2
-fit(train_ds,epochs,discriminator,generator_optimizer,discriminator_optimizer)
+epochs = 1
+fit(train_ds,epochs,discriminator,generator_optimizer,discriminator_optimizer,checkpoint,checkpoint_prefix)
 
-
+model_filename = 'model_trained.keras'    
+save_model(generator,model_filename,BUCKET_NAME)
 # # history = train(generator,train_ds,test_ds,epochs=epochs)
 
 # save_dir = '/opt/colorizer/'
